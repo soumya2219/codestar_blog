e88f588acbc2d068a2edf352949787ac4b4c438e
Soumya2219 <uv9845@gmail.com>
2025-09-15 15:41:13 +0100

    deployment settings applied, procfile created

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fnv%m=kyioyg0*7z43!t_fuf6i6#=swadm$btznbor1bqe2jm&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
